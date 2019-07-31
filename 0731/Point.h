d39ed3a2116b8214de08a1f3a0dc3f4eda2b4a2b
ChadSZ <18895373521@163.com>
2019-07-31 22:10:31 +0800

    Add files via upload

---
+++ 0731/Point.h
@@ -0,0 +1,55 @@
+/*类和对象， 求任意3点构成的三角形面积*/
+//#ifndef _POINT_H
+//#define _POINT_H
+//
+//#include <math.h>
+
+//class Point
+//{
+//private:
+//	float xCord, yCord;
+//public:
+//	Point() :xCord(0), yCord(0){} //利用无参数构建函数初始化 xCord 和 yCord
+//	~Point();
+//	Point(double, double);
+//	double getxCord();
+//	double getyCord();
+//	//double getLenth();
+//};
+//class triangle{
+//public:
+//	double L; //第一条边长
+//	double M; //第一条边长
+//	double N; //第一条边长
+//	// triangle() :L(0),M(0),N(0){}
+//	triangle();
+//	~triangle();
+//	void getCord(Point u[]);
+//	double getarea();
+//	double getlength();
+//
+//};
+//
+
+//#endif
+
+
+// Point.h
+
+#ifndef _POINT_H
+#define _POINT_H
+
+#include <math.h>
+
+class Point
+{
+private:
+	float x, y;
+public:
+	Point(double a, double b){ x = a; y = b; }
+	void print();
+	friend double pointDistance(Point p1, Point p2);
+	friend double area(Point p1, Point p2, Point p3);
+};
+
+#endif
---
+++ 0731/point.cpp
@@ -0,0 +1,58 @@
+// Point.cpp
+
+#include <iostream>
+using namespace std;
+
+#include "Point.h"
+
+void Point::print()
+{
+	cout << "(" << x << "," << y << ")" << " ";
+}
+
+double pointDistance(Point p1, Point p2)
+{
+	double distance = 0;
+	distance = sqrt((p1.y - p2.y)*(p1.y - p2.y) + (p1.x - p2.x)*(p1.x - p2.x));
+	return distance;
+}
+
+double area(Point p1, Point p2, Point p3)
+{
+	double area = 0;
+	double a = 0, b = 0, c = 0, s = 0;
+	a = pointDistance(p1, p2);
+	b = pointDistance(p2, p3);
+	c = pointDistance(p1, p3);
+	s = 0.5*(a + b + c);
+	area = sqrt(s*(s - a)*(s - b)*(s - c));
+	return area;
+}
+// PointMain.cpp
+
+#include <iostream>
+using namespace std;
+
+#include "Point.h"
+
+int main()
+{
+	double a, b;
+	cout << "请输入第一个点的坐标：" << endl;
+	cin >> a >> b ;
+	Point p1(a, b);
+	cout << "请输入第二个点的坐标：" << endl;
+	cin >> a >> b;
+	Point p2(a, b);
+	cout << "请输入第三个点的坐标：" << endl;
+	cin >> a >> b;
+	Point p3(a, b);
+	cout << "三角形顶点坐标为：";
+	p1.print();
+	p2.print();
+	p3.print();
+	cout << endl;
+	cout << "三角形面积为：";
+	cout << area(p1, p2, p3) << endl;
+	return 0;
+}
